b7d7d3b0a6414f715d2bdce25bd7136b50add49d
David Schmitt <david.schmitt@puppet.com>
2018-09-24 14:36:34 +0100

    Update notes on pdk cleanup

--- 2018/puppetize-live/cleanup.sh
+++ 2018/puppetize-live/cleanup.sh
@@ -2,5 +2,9 @@ cd ~
 rm -Rf ~/Projects ~/Downloads/pdk*deb
 mkdir ~/Projects
 cd ~/Projects
+sudo dpkg --purge pdk
 echo "switch this screen into tabbed mode"
-echo "switch VSCode puppet.installMode setting to agent"
+echo "up the font size of your terminal"
+echo "switch VSCode puppet.installType setting to agent"
+echo "get a shell, and a browser window"
+echo "clean other browser tabs, downloads"
